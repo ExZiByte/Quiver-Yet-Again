6dcf29b9c7871781c8aa5f699940d7df79f990bc
ExZiByte <exzibyte@gmail.com>
2022-01-02 16:35:35 -0600

    Added Guild Configuration reader

---
+++ src/main/java/me/exzibyte/Utilities/GuildConfig.java
@@ -0,0 +1,58 @@
+package me.exzibyte.Utilities;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import me.exzibyte.Quiver;
+import org.bson.Document;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.HashMap;
+
+public class GuildConfig {
+
+    private final Quiver quiver;
+    private static Logging logging = new Logging();
+
+    public GuildConfig(Quiver quiver){
+        this.quiver = quiver;
+    }
+
+    public HashMap<String, HashMap<String, String>> configuration = new HashMap<String, HashMap<String, String>>();
+
+    public void load(){
+
+        MongoCollection<Document> guilds =  quiver.getDatabase().getCollection("guilds");
+
+        FindIterable<Document> iterable = guilds.find();
+        MongoCursor<Document> cursor = iterable.iterator();
+
+        try{
+            while(cursor.hasNext()){
+                JSONParser parser = new JSONParser();
+                JSONObject obj = (JSONObject) parser.parse(cursor.next().toJson());
+
+                HashMap<String, String> data = new HashMap<String, String>();
+                data.put("locale", obj.get("locale").toString());
+                data.put("prefix", obj.get("prefix").toString());
+                data.put("logChannel", obj.get("logChannelID").toString());
+                data.put("joinLog", obj.get("joinLogID").toString());
+
+                configuration.put(obj.get("guildID").toString(), data);
+
+            }
+        } catch (ParseException ex){
+            logging.error(this.getClass(), ex.toString());
+        } finally {
+            configuration.forEach((k,v) -> {
+               logging.info(this.getClass(), String.format("Key: %s, Value: %s", k, v));
+            });
+            cursor.close();
+        }
+
+
+    }
+
+}
