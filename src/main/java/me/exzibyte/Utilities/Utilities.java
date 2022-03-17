000b3d611f97acff24ba73ddc2b6a64c1acfee96
ExZiByte <exzibyte@gmail.com>
2022-03-17 18:25:18 -0500

    Added a Utilities class for storage of reused methods

---
+++ src/main/java/me/exzibyte/Utilities/Utilities.java
@@ -0,0 +1,16 @@
+package me.exzibyte.Utilities;
+
+import net.dv8tion.jda.api.JDA;
+
+public class Utilities {
+
+    public int successGreen = 0x54c72a;
+    public int warningYellow = 0xe6da35;
+    public int failedRed = 0xd63f3a;
+
+    public String getEmote(String emoteID, JDA jda){
+        return jda.getEmoteById(emoteID).getAsMention();
+    }
+
+
+}
