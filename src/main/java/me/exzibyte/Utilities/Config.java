078ab308630b1ea43d27e83f1aeb1a4fcd09dd3f
ExZiByte <exzibyte@gmail.com>
2022-03-28 20:11:20 -0500

    Added a few extra config options

--- src/main/java/me/exzibyte/Utilities/Config.java
+++ src/main/java/me/exzibyte/Utilities/Config.java
@@ -1,6 +1,7 @@
 package me.exzibyte.Utilities;
 
 import me.exzibyte.Quiver;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -14,6 +15,7 @@ public class Config {
     private final Quiver quiver;
     Logging logging = new Logging();
     public HashMap<String, String> configuration = new HashMap<>();
+    public HashMap<String, JSONArray> conf = new HashMap<>();
     private static HashMap<String, String> locales = new HashMap<>();
 
     public Config(Quiver quiver){
@@ -25,10 +27,13 @@ public class Config {
         try(FileReader reader = new FileReader("config.json")){
             JSONObject obj = (JSONObject) parser.parse(reader);
 
+            configuration.put("mode", obj.get("mode").toString());
             configuration.put("token", obj.get("token").toString());
             configuration.put("dbURI", obj.get("databaseURI").toString());
             configuration.put("shardCount", obj.get("shardCount").toString());
             configuration.put("disabledCommands", obj.get("disabledCommands").toString());
+            configuration.put("quiverAdminWebhook", obj.get("quiverAdminWebhook").toString());
+            conf.put("allowedGuilds", (JSONArray) obj.get("allowedGuilds"));
             //locales.put("availableLocales", obj.get("locales").toString());
 
         } catch(IOException | ParseException exception){
@@ -38,6 +43,15 @@ public class Config {
 
     public String get(String key){
         return configuration.get(key);
+    }
+
+    public boolean isGuildAllowed(String guildID){
+        if(conf.containsValue(guildID)) return true;
+        return false;
+    }
+    public boolean isGuildAllowed(Long guildID){
+        if(conf.containsValue(guildID)) return true;
+        return false;
     }
 
 }
